c14a0d6681a5f24aa0c01410dd357605d39947d6
Ganesh M <ganeshroyal512@gmail.com>
2024-01-06 13:10:31 +0530

    first commit

---
+++ app/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+
+# Create your views here.
+def srujana(request):
+    return render(request,'srujana.html')
+def ballu(request):
+    return render(request,'ballu.html')
